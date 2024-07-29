625380c9a956a39b7d83d82f94bb5c81f583d426
SpicyCoder <spk2naveen@hotmail.com>
2024-07-29 20:55:58 +0530

    Add Aspire Dashboard

--- Subscriber/Program.cs
+++ Subscriber/Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.AddServiceDefaults();
 builder.Services.AddDaprClient();
 builder.Services.AddControllers().AddDapr();
 builder.Services.AddEndpointsApiExplorer();
